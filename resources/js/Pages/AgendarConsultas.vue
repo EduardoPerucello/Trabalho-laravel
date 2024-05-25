575e02d8bf7d498f77f655be265ad342ff7802d8
Eduardo de Faria Rios Perucello <115468808+EduardoPerucello@users.noreply.github.com>
2024-05-25 11:40:04 -0300

    Update AgendarConsultas.vue

--- resources/js/Pages/AgendarConsultas.vue
+++ resources/js/Pages/AgendarConsultas.vue
@@ -5,32 +5,48 @@ import { Head } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
-    <Head title="Agendar Consulta" />
+    <Head title="Cliente" />
 
-    <div class="container mx-auto p-6">
-        <h1 class="text-2xl font-semibold mb-4">Agendar Consulta</h1>
-        <form @submit.prevent="submitForm" class="max-w-md mx-auto">
-            <!-- Campos do formulário -->
-            <div class="mb-4">
-                <label for="patientId" class="block text-sm font-medium text-gray-700">Paciente</label>
-                <input type="text" v-model="patientId" id="patientId" placeholder="ID do Paciente" class="mt-1 p-2 border rounded-md w-full">
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Cliente
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="container mx-auto p-6">
+                        <h1 class="text-2xl font-semibold mb-4">Agendar Consulta</h1>
+                        <form @submit.prevent="submitForm" class="max-w-md mx-auto">
+                            <!-- Campos do formulário -->
+                            <div class="mb-4">
+                                <label for="patientId" class="block text-sm font-medium text-gray-700">Paciente</label>
+                                <input type="text" v-model="patientId" id="patientId" placeholder="ID do Paciente" class="mt-1 p-2 border rounded-md w-full">
+                            </div>
+                            <div class="mb-4">
+                                <label for="psicologoId" class="block text-sm font-medium text-gray-700">Psicólogo</label>
+                                <input type="text" v-model="psicologoId" id="psicologoId" placeholder="ID do Psicólogo" class="mt-1 p-2 border rounded-md w-full">
+                            </div>
+                            <div class="mb-4">
+                                <label for="date" class="block text-sm font-medium text-gray-700">Data</label>
+                                <input type="date" v-model="date" id="date" class="mt-1 p-2 border rounded-md w-full">
+                            </div>
+                            <div class="mb-6">
+                                <label for="time" class="block text-sm font-medium text-gray-700">Horário</label>
+                                <input type="time" v-model="time" id="time" class="mt-1 p-2 border rounded-md w-full">
+                            </div>
+                            <div v-if="errorMessage" class="text-red-500 mb-4">{{ errorMessage }}</div>
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Agendar</button>
+                        </form>
+                    </div>
+                    </div>
+                </div>
             </div>
-            <div class="mb-4">
-                <label for="psicologoId" class="block text-sm font-medium text-gray-700">Psicólogo</label>
-                <input type="text" v-model="psicologoId" id="psicologoId" placeholder="ID do Psicólogo" class="mt-1 p-2 border rounded-md w-full">
-            </div>
-            <div class="mb-4">
-                <label for="date" class="block text-sm font-medium text-gray-700">Data</label>
-                <input type="date" v-model="date" id="date" class="mt-1 p-2 border rounded-md w-full">
-            </div>
-            <div class="mb-6">
-                <label for="time" class="block text-sm font-medium text-gray-700">Horário</label>
-                <input type="time" v-model="time" id="time" class="mt-1 p-2 border rounded-md w-full">
-            </div>
-            <div v-if="errorMessage" class="text-red-500 mb-4">{{ errorMessage }}</div>
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Agendar</button>
-        </form>
-    </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
 
 <script>
@@ -76,3 +92,6 @@ export default {
     }
 }
 </script>
+
+
+
